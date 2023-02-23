207b790aa6845b51cb4acc78f006083442ccc0d5
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-02-24 07:02:47 +1100

    coin change top down

--- coinChange.cpp
+++ coinChange.cpp
@@ -3,24 +3,29 @@
 
 using namespace std;
 
-// bottom up
-int minCoinChange(vector<int> &denoms, int change) {
-    vector<int> dp(change+1, INT_MAX);
-    dp[0] = 0;
-    for (int i = 1; i <= change; i++) {
-        for (int c: denoms) {
-            if (i - c >= 0 && dp[i-c] != INT_MAX) {
-                dp[i] = min(dp[i-c]+1, dp[i]);
-            }
+// top down
+int minCoinChange(vector<int> &denoms, int change, vector<int> &dp) {
+    // base case
+    if (change == 0) return 0;
+    if (change < 0) return 0;
+    if (dp[change] != INT_MAX) return dp[change];
+
+    int ans = INT_MAX;
+    for (int c: denoms) {
+        if (change - c >= 0) {
+            ans = min( ans, minCoinChange(denoms, change-c, dp) + 1 );
         }
     }
-    
-    return dp[change] != INT_MAX ? dp[change] : -1;
+   
+    dp[change] = ans;
+    return dp[change];
 }
 
 int main(void) {
     vector<int> denoms = {1,5,7,10};
-    cout << minCoinChange(denoms, 16) << endl;
+    vector<int> dp(16+1, INT_MAX);
+    dp[0] = 0;
+    cout << minCoinChange(denoms, 16, dp) << endl;
 
     return 0;
 }
