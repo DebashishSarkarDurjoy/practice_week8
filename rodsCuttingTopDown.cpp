cde1dc43eb7c8b42a20a29d32834489910a846b7
DebashishSarkarDurjoy <dsdjoy4@gmail.com>
2023-02-24 12:05:17 +1100

    rods cutting top down

--- rodsCuttingTopDown.cpp
+++ rodsCuttingTopDown.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 
 int maxProfit(int *prices, int n, vector<int> &dp) {
-    
-    return 10;
+    if (n == 0) return 0;
+    if (n < 0) return 0;
+    if (dp[n] != 0) return dp[n];
+
+    int ans = INT_MIN;
+    for (int i = 1; i <= n; i++) {
+        int current = prices[i-1] + maxProfit(prices, n-i, dp);
+        ans = max(ans, current);
+    }
+    dp[n] = ans;
+    return dp[n];
 }
 
 int main(void) {
